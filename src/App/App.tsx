c801fac012956960dddb84c90236e8ec1756e453
dmuller189 <muller.d@husky.neu.edu>
2019-09-06 14:54:47 -0400

    added divider for d3

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -91,8 +91,8 @@ export default class App extends React.Component<{}, IState, null> {
                             width="30"
                             height="32"
                             viewBox="0 -9 30 33">
-                              <g id="layer2" transform="translate(1, 0)">
-                            <path d="M22 18.055v2.458c0 1.925-4.655 3.487-10 3.487-5.344 0-10-1.562-10-3.487v-2.458c2.418 1.738 7.005 2.256 10 2.256 3.006 0 7.588-.523 10-2.256zm-10-3.409c-3.006 0-7.588-.523-10-2.256v2.434c0 1.926 4.656 3.487 10 3.487 5.345 0 10-1.562 10-3.487v-2.434c-2.418 1.738-7.005 2.256-10 2.256zm0-14.646c-5.344 0-10 1.562-10 3.488s4.656 3.487 10 3.487c5.345 0 10-1.562 10-3.487 0-1.926-4.655-3.488-10-3.488zm0 8.975c-3.006 0-7.588-.523-10-2.256v2.44c0 1.926 4.656 3.487 10 3.487 5.345 0 10-1.562 10-3.487v-2.44c-2.418 1.738-7.005 2.256-10 2.256z" />
+                            <g id="layer2" transform="translate(1, 0)">
+                              <path d="M22 18.055v2.458c0 1.925-4.655 3.487-10 3.487-5.344 0-10-1.562-10-3.487v-2.458c2.418 1.738 7.005 2.256 10 2.256 3.006 0 7.588-.523 10-2.256zm-10-3.409c-3.006 0-7.588-.523-10-2.256v2.434c0 1.926 4.656 3.487 10 3.487 5.345 0 10-1.562 10-3.487v-2.434c-2.418 1.738-7.005 2.256-10 2.256zm0-14.646c-5.344 0-10 1.562-10 3.488s4.656 3.487 10 3.487c5.345 0 10-1.562 10-3.487 0-1.926-4.655-3.488-10-3.488zm0 8.975c-3.006 0-7.588-.523-10-2.256v2.44c0 1.926 4.656 3.487 10 3.487 5.345 0 10-1.562 10-3.487v-2.44c-2.418 1.738-7.005 2.256-10 2.256z" />
                             </g>
                           </svg>
                         </div>
@@ -115,7 +115,7 @@ export default class App extends React.Component<{}, IState, null> {
                             viewBox="400 160 300 400">
                             <g id="layer3" transform="translate(45, 130)">
                               <path d="M620,305.666v-51.333l-31.5-5.25c-2.333-8.75-5.833-16.917-9.917-23.917L597.25,199.5l-36.167-36.75l-26.25,18.083  c-7.583-4.083-15.75-7.583-23.916-9.917L505.667,140h-51.334l-5.25,31.5c-8.75,2.333-16.333,5.833-23.916,9.916L399.5,163.333  L362.75,199.5l18.667,25.666c-4.083,7.584-7.583,15.75-9.917,24.5l-31.5,4.667v51.333l31.5,5.25  c2.333,8.75,5.833,16.334,9.917,23.917l-18.667,26.25l36.167,36.167l26.25-18.667c7.583,4.083,15.75,7.583,24.5,9.917l5.25,30.916  h51.333l5.25-31.5c8.167-2.333,16.333-5.833,23.917-9.916l26.25,18.666l36.166-36.166l-18.666-26.25  c4.083-7.584,7.583-15.167,9.916-23.917L620,305.666z M480,333.666c-29.75,0-53.667-23.916-53.667-53.666s24.5-53.667,53.667-53.667  S533.667,250.25,533.667,280S509.75,333.666,480,333.666z" />
-                           </g>
+                            </g>
                           </svg>
                         </div>
                         <div className="col-sm">
@@ -139,7 +139,7 @@ export default class App extends React.Component<{}, IState, null> {
                         <div className="col-sm">
                           <span className="list-title">Export:</span>
                           <br></br>
-                           Deploy and share your visual creations with the world!
+                          Deploy and share your visual creations with the world!
                         </div>
                       </div>
                     </li>
@@ -156,7 +156,7 @@ export default class App extends React.Component<{}, IState, null> {
                   field2="password"
                   forgotPassword="true"
                   submitMsg="Log-in"
-                  submitT="outline-" />              
+                  submitT="outline-" />
               </div>
               <div id="right-content-main" className="justify-content-center">
                 <h2>
@@ -166,15 +166,28 @@ export default class App extends React.Component<{}, IState, null> {
                 <br></br>
                 <br></br>
                 <h6>Join!</h6>
-                <button id="main-sign"type="button" className="btn btn-primary btn-block round">Sign-up</button>          
+                <button id="main-sign" type="button" className="btn btn-primary btn-block round">Sign-up</button>
                 <button id="main-log" type="button" className="btn btn-outline-primary btn-block round">Log-in</button>
-                <button id="main-guest" type="button" className="btn btn-outline-dark round">Continue as Guest</button>      
+                <button id="main-guest" type="button" className="btn btn-outline-dark round">Continue as Guest</button>
               </div>
             </div>
           </div>
         </div>
         <div className="divider">
-          <Pie1 />
+          {/* <Pie1 /> */}
+          <div className="row justify-content-center" id="divider-header">
+            <div className="justify-content-between">
+              <div className="d-flex flex-column">
+                <h1>
+                  Complex Visualization Simplified
+                </h1>
+                <h4>
+                  made possible with DollarFlare
+                </h4>
+                <Pie1 />
+              </div>
+            </div>
+          </div>
         </div>
         <div className="row h-100" id="second-row-left">
           <div className="col-6 no-float">
@@ -183,7 +196,7 @@ export default class App extends React.Component<{}, IState, null> {
                 <h2>
                   Join our mailing list fot the latest features and updates <br></br>
                 </h2>
-                <LoginForm field1="name" field2="Email" submitMsg="Join!"/>
+                <LoginForm field1="name" field2="Email" submitMsg="Join!" />
                 <br></br>
               </div>
               <div id="left-content-body">
