f3a7f31e62654b5f3f264d2839989bbcd2591ecf
unknown <muller.d@husky.neu.edu>
2019-09-15 20:50:12 -0400

    renaming

--- src/App/ForexGraph/ForexGraphLanding.tsx
+++ src/App/ForexGraph/ForexGraphLanding.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import GraphViewController from './FXGraphControllerShell/GraphViewController';
+import './ForexGraphLanding.css';
 
 
 
-//landing page for when user navigate to the forex graph network view, contianin a
+
+// landing page for when user navigate to the forex graph network view, contianin 
+// all styling, formatting, and functionality, including the GraphViewController shell and the d3 model itself
+
 export default class ForexGraph extends React.Component {
 
     render() {
         return (
             <div>
-                Forex grpah home
+                Forex graph landing page, controller shell bellow
                 <br>
                 </br>
                 <GraphViewController />
