af9fa7eee0d28692111f9b229d7a413883a02bac
unknown <muller.d@husky.neu.edu>
2019-09-17 14:34:44 -0400

    reduxing

--- src/index.tsx
+++ src/index.tsx
@@ -8,17 +8,29 @@ import Login from './App/LoginPage/Login';
 import ForgotPassword from './App/ForgotPasswordPage/ForgotPassword';
 import Home from './App/homePage/home';
 
+import { createStore, combineReducers, applyMiddleware  } from 'redux';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
 
-ReactDOM.render((
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={LandingPage}></Route>
-        <Route exact path="/sign-up" component={SignUp}></Route>
-        <Route exact path="/login" component={Login}></Route>
-        <Route exact path="/forgotPassword" component={ForgotPassword}></Route>
-        <Route exact path="/home" component={Home}></Route>
-      </Switch>  
-    </BrowserRouter>),
+
+
+const rootReducer = combineReducers({});
+
+const store = createStore(rootReducer, applyMiddleware(createLogger()));
+
+
+ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={LandingPage}></Route>
+          <Route exact path="/sign-up" component={SignUp}></Route>
+          <Route exact path="/login" component={Login}></Route>
+          <Route exact path="/forgotPassword" component={ForgotPassword}></Route>
+          <Route exact path="/home" component={Home}></Route>
+        </Switch>  
+      </BrowserRouter>)
+    </Provider>,
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
