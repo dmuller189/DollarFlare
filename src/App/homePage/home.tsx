41ee82e4b05a058d3dec4a5866487d65213b6110
unknown <muller.d@husky.neu.edu>
2019-09-19 08:56:12 -0400

    utilities

--- src/App/homePage/home.tsx
+++ src/App/homePage/home.tsx
@@ -2,60 +2,29 @@ import React from 'react';
 import './home.css';
 import { connect } from 'react-redux';
 import Footer from '../utilityComponents/footer';
-import github_logo from '../d3Visuals_and_icons/socialMedia/SMicons/github_logo.svg';
-import { LoginForm } from '../utilityComponents/LoginForm/LoginForm';
-
+import NavBar from '../utilityComponents/NavBar';
 
 //home page after login/coninuing as guest
 class Home extends React.Component {
-
-
 
 
   render() {
 
     return (
       <div id="home-root">
+
         <div className="container" id="main-bar" >
-          <nav className="navbar navbar-expand-lg navbar-light" role="navigation" >
-            
-            <div className="nav navbar-nav" >
-              <a href="#" id="logo">
-                <div className="container">
-                  <img src={github_logo} className="home-img"/>
-                </div>
-              </a>
-
-
-              <ul className="navbar-nav mr-auto mt-2 mt-lg-0" >
-                <li className="nav-item active">
-                  <a className="nav-link" href="#">About</a>
-                </li>
-                <li className="nav-item active">
-                  <a className="nav-link" href="#">Gallery</a>
-                </li>
-                <li>
-                  <a className="nav-link active" href="#">Learning</a>
-                </li>
-                <li>
-                  <a className="nav-link active" href="#">FAQ</a>
-                </li>
-                <li>
-                  <a href="https://github.ccs.neu.edu/dmuller189/DollarFlare/" id="gitlogo">
-                    <img src={github_logo} id="githubLogo" className="home-img" />
-                  </a>
-                </li>     
-              </ul>      
-            </div>
-          </nav>
+          <NavBar />
         </div>
 
         <div id="page-content">
           content
-        </div>
+          </div>
+
         <div id="page-footer">
           <Footer />
         </div>
+
       </div>
     )
   }
---
+++ src/App/utilityComponents/NavBar.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+
+export default class NavBar extends React.Component {
+
+
+
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-primary">
+
+                <a className="navbar-brand" href="#">Navbar</a>
+
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
+                    aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="basicExampleNav">
+
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home
+                <span className="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Features</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Pricing</a>
+                        </li>
+                    </ul>
+                    <form className="form-inline">
+                        <div className="md-form my-0">
+                            <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+                        </div>
+                    </form>
+                </div>
+            </nav>
+        )
+    }
+}
