1faeac58f4f2a9434af79506bedd81b101caa2fe
unknown <muller.d@husky.neu.edu>
2019-09-27 13:52:10 -0400

    added community component

--- src/App/LoggedInContainers/CommunityPage/CommunityPage.tsx
+++ src/App/LoggedInContainers/CommunityPage/CommunityPage.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 
 
-class Profile extends React.Component {
+class CommunityPage extends React.Component {
+
 
 
     render() {
         return (
             <div>
                 <h1>
-                    Profile Container
+                    Community Container
                 </h1>
             </div>
         )
     }
 }
-
-export default Profile;
