b6cb99a676983550f56036f0bc526f991c11c63c
unknown <muller.d@husky.neu.edu>
2019-10-01 16:31:24 -0400

    home page work

--- src/App/LoggedInContainers/HomePage/HomePage.tsx
+++ src/App/LoggedInContainers/HomePage/HomePage.tsx
@@ -4,15 +4,30 @@ import { Link } from "react-router-dom";
 import './HomePage.css';
 
 
+
 class HomePage extends React.Component {
 
     render() {
         return (
             <div>
-                <h1>
-                    HomePage Container
-                </h1>
-                
+
+                <div className="center-div" id="page-header">
+                    <h1>
+                        Welcome to DollarFlare!
+                    </h1>
+                    <br></br>
+                    <h4>
+                        Choose a template to begin your creation!
+                    </h4>
+                </div>
+
+
+
+
+
+
+
+
             </div>
         )
     }
--- src/App/LoggedInContainers/profilePage/Profile.tsx
+++ src/App/LoggedInContainers/profilePage/Profile.tsx
@@ -5,10 +5,16 @@ class Profile extends React.Component {
 
 
     render() {
+        /**
+         * iterate through saved views and display 
+         * them in a grid showing title, date last modified,
+         * and a generated screen shot of the view
+         * 
+         */
         return (
             <div>
                 <h1>
-                    Profile Container
+                    Welcome Back, User!
                 </h1>
             </div>
         )
