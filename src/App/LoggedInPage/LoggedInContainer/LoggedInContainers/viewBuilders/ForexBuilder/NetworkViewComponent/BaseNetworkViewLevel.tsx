72830d7a5753c7ac924ca288fe86b3a83a45cb10
unknown <muller.d@husky.neu.edu>
2020-01-01 20:34:38 -0500

    the start of something graph

--- src/App/LoggedInPage/LoggedInContainer/LoggedInContainers/viewBuilders/ForexBuilder/NetworkViewComponent/BaseNetworkViewLevel.tsx
+++ src/App/LoggedInPage/LoggedInContainer/LoggedInContainers/viewBuilders/ForexBuilder/NetworkViewComponent/BaseNetworkViewLevel.tsx
@@ -1,8 +1,97 @@
 import React from 'react';
 import { IGraph } from '../forexGraphStructureAndLogic/GraphDataModelandLogic';
+//@ts-ignore
+import { Graph } from 'react-d3-graph';
+
+
+const data = {
+    nodes: [{ id: "Harry",
+            color: "red" }, 
+
+            { id: "Sally",
+            color: "blue" }, 
+
+            { id: "Alice" }],
+    links: [{ source: "Harry", target: "Sally" }, { source: "Harry", target: "Alice" }],
+};
+const myConfig = {
+    nodeHighlightBehavior: true,
+    node: {
+        color: "lightgreen",
+        size: 1000,
+        highlightStrokeColor: "blue",
+    },
+    link: {
+        highlightColor: "lightblue",
+    },
+};
+
+const onClickGraph = function () {
+  //  window.alert(`Clicked the graph background`);
+};
+
+//@ts-ignore
+const onClickNode = function (nodeId) {
+   // window.alert(`Clicked node ${nodeId}`);
+};
+//@ts-ignore
+
+const onDoubleClickNode = function (nodeId) {
+   // window.alert(`Double clicked node ${nodeId}`);
+};
+//@ts-ignore
+
+const onRightClickNode = function (event, nodeId) {
+   // window.alert(`Right clicked node ${nodeId}`);
+};
+//@ts-ignore
+
+const onMouseOverNode = function (nodeId) {
+ //   window.alert(`Mouse over node ${nodeId}`);
+};
+//@ts-ignore
+
+const onMouseOutNode = function (nodeId) {
+ //   window.alert(`Mouse out node ${nodeId}`);
+};
+//@ts-ignore
+
+const onClickLink = function (source, target) {
+  //  window.alert(`Clicked link between ${source} and ${target}`);
+};
+//@ts-ignore
+
+const onRightClickLink = function (event, source, target) {
+  //  window.alert(`Right clicked link between ${source} and ${target}`);
+};
+//@ts-ignore
+
+const onMouseOverLink = function (source, target) {
+  //  window.alert(`Mouse over in link between ${source} and ${target}`);
+};
+//@ts-ignore
+
+const onMouseOutLink = function (source, target) {
+   // window.alert(`Mouse out link between ${source} and ${target}`);
+};
+//@ts-ignore
+
+const onNodePositionChange = function (nodeId, x, y) {
+  //  window.alert(`Node ${nodeId} is moved to new position. New position is x= ${x} y= ${y}`);
+};
+
 
 interface IProps {
     model: IGraph
+}
+
+interface IState {
+    graph: { id: number, label: string }[],
+    options: any,
+    events: any,
+    style: any,
+
+
 }
 
 //class representing the drawing library to render the forex model
@@ -19,34 +108,57 @@ interface IProps {
 //  - clear selected nodes (and thier respective out-edges)
 //  - add all nodes
 //  - add all edges 
-export default class BaseNetworkViewLevel extends React.Component <IProps> {
+export default class BaseNetworkViewLevel extends React.Component<IProps> {
 
     constructor(props: IProps) {
         super(props);
+    }
+
+    componentDidMount() {
+    }
+
+    drawForex(): void {
     }
 
     render() {
         return (
 
             <div className="d-flex justify-content-center">
-              <h1>
+                {/* <h1>
                     Drawing layer
-              </h1>
-              
-              <ul>
-                  {this.props.model.nodeList.map(
-                      e => {
-                          return (
-                              <li>
-                                  
-                                 {e.name + " -> " + e.neighbors.map(
-                                     j => j.toNode.name + ", "
-                                 )} 
-                              </li>
-                          )
-                      }
-                  )}
-              </ul>
+              </h1> */}
+{/* 
+                <ul>
+                    {this.props.model.nodeList.map(
+                        e => {
+                            return (
+                                <li>
+
+                                    {e.name + " -> " + e.neighbors.map(
+                                        j => j.toNode.name + ", "
+                                    )}
+                                </li>
+                            )
+                        }
+                    )}
+                </ul> */}
+                <div id="mynetwork"></div>
+
+                <Graph
+                    id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
+                    data={data}
+                    config={myConfig}
+                    onClickNode={onClickNode}
+                    onRightClickNode={onRightClickNode}
+                    onClickGraph={onClickGraph}
+                    onClickLink={onClickLink}
+                    onRightClickLink={onRightClickLink}
+                    onMouseOverNode={onMouseOverNode}
+                    onMouseOutNode={onMouseOutNode}
+                    onMouseOverLink={onMouseOverLink}
+                    onMouseOutLink={onMouseOutLink}
+                    onNodePositionChange={onNodePositionChange}
+                />
             </div>
         )
     }
